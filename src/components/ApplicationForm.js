801f8ba3f5ec97d9b07a00f08eb2eb6bc60bf0ad
Rudhra Raveendran <rudytheninja@gmail.com>
2020-07-23 17:20:44 -0400

    Fixed bsCustomFileInput init

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import bsCustomFileInput from "bs-custom-file-input";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -80,114 +79,139 @@ const renderQuestion = (question, disabled = false) => {
   );
 };
 
-const ApplicationForm = ({ firebase }) => {
-  const [applicationFormConfig, setApplicationFormConfig] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [validated, setValidated] = useState(false);
-  const [sending, setSending] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("");
-
-  useEffect(() => {
-    const loadApplicationFormConfig = async () => {
-      const doc = await firebase.applicationFormConfig().get();
-
-      if (!doc.exists) {
-        setErrorMsg(
-          'Document "applicationFormConfig" does not exist! Please inform an administrator.'
-        );
-        setLoading(false);
-      } else {
-        setApplicationFormConfig(doc.data());
-      }
-
-      setLoading(false);
-    };
-    if (firebase) loadApplicationFormConfig();
-  }, [firebase]);
-
-  if (loading) return <Loader />;
-
-  const onSubmit = async (event) => {
-    setSending(true);
-    event.preventDefault();
-    const form = event.currentTarget;
-    const fileUpload = form.querySelector(".custom-file-input");
-    setFileValidity(fileUpload);
-
-    if (form.checkValidity() === false) {
-      event.stopPropagation();
-    } else {
-      const inputs = form.querySelectorAll(".form-control");
-
-      console.log(inputs);
-      console.log(fileUpload);
-      // save to firebase
-
-      setSubmitted(true);
-    }
-
-    setSending(false);
-    setValidated(true);
+class ApplicationForm extends React.Component {
+  state = {
+    applicationFormConfig: null,
+    loading: true,
+    validated: false,
+    sending: false,
+    submitted: false,
+    errorMsg: "",
   };
 
-  const successMessage = (
-    <Container flexdirection="column">
-      <div style={{ alignSelf: "center" }}>
-        <Logo size="medium" />
-      </div>
+  loadApplicationFormConfig = async () => {
+    const doc = await this.props.firebase.applicationFormConfig().get();
 
-      <h1>Application Submitted!</h1>
-      <p>
-        Thank you for applying to join BU UPE. Please check your email for a
-        confirmation of your submission. Further details, such as interview
-        timeslots, will be prompted via email and can be entered in this
-        application. If you'd like to edit your submission, simply refresh this
-        page and re-apply.
-      </p>
-    </Container>
-  );
+    if (!doc.exists) {
+      this.setState({
+        errorMsg:
+          'Document "applicationFormConfig" does not exist! Please inform an administrator.',
+        loading: false,
+      });
+    } else {
+      this.setState({
+        applicationFormConfig: doc.data(),
+        loading: false,
+      });
+    }
+  };
 
-  if (errorMsg)
-    return (
+  componentDidUpdate(prevProps) {
+    if (this.props.firebase !== null) {
+      this.loadApplicationFormConfig();
+    }
+    if (typeof window !== "undefined") {
+      import("bs-custom-file-input").then((bsCustomFileInput) => {
+        bsCustomFileInput.init();
+      });
+    }
+  }
+
+  render() {
+    const {
+      loading,
+      errorMsg,
+      submitted,
+      validated,
+      applicationFormConfig,
+      sending,
+    } = this.state;
+
+    if (loading) return <Loader />;
+
+    const onSubmit = async (event) => {
+      this.setState({ sending: true });
+      event.preventDefault();
+      const form = event.currentTarget;
+      const fileUpload = form.querySelector(".custom-file-input");
+      setFileValidity(fileUpload);
+
+      if (form.checkValidity() === false) {
+        event.stopPropagation();
+      } else {
+        const inputs = form.querySelectorAll(".form-control");
+
+        console.log(inputs);
+        console.log(fileUpload);
+        // save to firebase
+
+        this.setState({ submitted: true });
+      }
+
+      this.setState({
+        sending: false,
+        validated: true,
+      });
+    };
+
+    const successMessage = (
       <Container flexdirection="column">
-        <h1>Uh oh!</h1>
-        <p>{errorMsg}</p>
+        <div style={{ alignSelf: "center" }}>
+          <Logo size="medium" />
+        </div>
+
+        <h1>Application Submitted!</h1>
+        <p>
+          Thank you for applying to join BU UPE. Please check your email for a
+          confirmation of your submission. Further details, such as interview
+          timeslots, will be prompted via email and can be entered in this
+          application. If you'd like to edit your submission, simply refresh
+          this page and re-apply.
+        </p>
       </Container>
     );
 
-  if (submitted) return successMessage;
+    if (errorMsg)
+      return (
+        <Container flexdirection="column">
+          <h1>Uh oh!</h1>
+          <p>{errorMsg}</p>
+        </Container>
+      );
 
-  bsCustomFileInput.init();
-  // use auth user context here, if user has already applied dont let them apply again (alternatively, just let them know they've applied already and further applications will overwrite previous ones)
-  return (
-    <Container flexdirection="column">
-      <CenteredForm noValidate validated={validated} onSubmit={onSubmit}>
-        <Logo size="medium" />
-        <h1>Apply to BU UPE</h1>
-        {/* maybe this message here should be configurable as well? */}
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-          fringilla, dui vitae maximus luctus, magna urna convallis purus,
-          condimentum ullamcorper velit dui eu dolor. Pellentesque et tincidunt
-          tellus. Fusce venenatis magna sed elit bibendum, sed scelerisque augue
-          placerat. Vestibulum nec mi efficitur, posuere nisl at, pretium ex.
-          Quisque quam dui, pulvinar a pellentesque eu, cursus et elit. Ut
-          volutpat imperdiet ex, id commodo nulla pretium id. Proin accumsan
-          dignissim tortor, id pulvinar urna euismod ac. Morbi suscipit massa id
-          dui feugiat ultrices. Nulla ac faucibus tortor, quis pharetra leo.
-        </p>
+    if (submitted) return successMessage;
 
-        {applicationFormConfig.questions
-          .sort((a, b) => (a.order > b.order ? 1 : -1))
-          .map((question) => renderQuestion(question))}
+    // use auth user context here, if user has already applied dont let them apply again (alternatively, just let them know they've applied already and further applications will overwrite previous ones)
+    return (
+      <Container flexdirection="column">
+        <CenteredForm noValidate validated={validated} onSubmit={onSubmit}>
+          <Logo size="medium" />
+          <h1>Apply to BU UPE</h1>
+          {/* maybe this message here should be configurable as well? */}
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+            fringilla, dui vitae maximus luctus, magna urna convallis purus,
+            condimentum ullamcorper velit dui eu dolor. Pellentesque et
+            tincidunt tellus. Fusce venenatis magna sed elit bibendum, sed
+            scelerisque augue placerat. Vestibulum nec mi efficitur, posuere
+            nisl at, pretium ex. Quisque quam dui, pulvinar a pellentesque eu,
+            cursus et elit. Ut volutpat imperdiet ex, id commodo nulla pretium
+            id. Proin accumsan dignissim tortor, id pulvinar urna euismod ac.
+            Morbi suscipit massa id dui feugiat ultrices. Nulla ac faucibus
+            tortor, quis pharetra leo.
+          </p>
 
-        <Button type="submit" disabled={sending}>
-          Submit
-        </Button>
-      </CenteredForm>
-    </Container>
-  );
-};
+          {applicationFormConfig.questions
+            .sort((a, b) => (a.order > b.order ? 1 : -1))
+            .map((question) => renderQuestion(question))}
+
+          <Button type="submit" disabled={sending}>
+            Submit
+          </Button>
+        </CenteredForm>
+      </Container>
+    );
+  }
+}
 
 export default withFirebase(ApplicationForm);
