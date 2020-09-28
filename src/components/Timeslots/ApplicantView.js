05036c249d1ddebd5ca2e07b58d87922fdf0f5ca
Rudhra Raveendran <rudytheninja@gmail.com>
2020-09-28 17:55:08 -0400

    Add await and error handlers to firebase function calls

--- src/components/Timeslots/ApplicantView.js
+++ src/components/Timeslots/ApplicantView.js
@@ -176,11 +176,12 @@ const ApplicantView = ({ firebase }) => {
       }
 
       if (action === "schedule") {
-        firebase.timeslotSelected({
+        await firebase.timeslotSelected({
           firstName: authUser.name.split(" ")[0],
           email: authUser.email,
           time: formatTime(timeslot.time),
-        });
+        })
+        .catch(error => console.error(error));
         swal({
           title: "You're all set!",
           content: (
@@ -195,10 +196,11 @@ const ApplicantView = ({ firebase }) => {
           icon: "success",
         });
       } else if (action === "unschedule") {
-        firebase.timeslotUnselected({
+        await firebase.timeslotUnselected({
           firstName: authUser.name.split(" ")[0],
           email: authUser.email,
-        });
+        })
+        .catch(error => console.error(error));
         swal(
           "Unscheduled!",
           `You have unscheduled your interview, and a confirmation has been sent to your email. If you'd still like to interview with UPE, make sure to select another timeslot!`,
