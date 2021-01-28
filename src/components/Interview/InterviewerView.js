f9112cd034881bad7e2475b6b112652edfe069fe
Rudhra Raveendran <rudytheninja@gmail.com>
2021-01-27 19:07:13 -0800

    Fix object path in application update

--- src/components/Interview/InterviewerView.js
+++ src/components/Interview/InterviewerView.js
@@ -214,9 +214,9 @@ class InterviewerView extends Component {
           // eslint-disable-next-line no-unused-vars
           const application = { ...doc.data() };
           const updateObject = {};
-          updateObject[`interview/notes/${this.context.uid}`] =
+          updateObject[`interview.notes.${this.context.uid}`] =
             interview.notes[this.context.uid];
-          updateObject[`interview/scores/${this.context.uid}`] =
+          updateObject[`interview.scores.${this.context.uid}`] =
             interview.scores[this.context.uid];
           transaction.update(ref, updateObject);
         }
