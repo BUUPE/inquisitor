c71974fc20650944ee3d4bdddeff0683acc0833c
vitorvicente <vitor@bu.edu>
2022-01-04 17:25:18 +0000

    404 Page UI Rework

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { Link } from "gatsby";
 
-import Logo from "../components/Logo";
 import SEO from "../components/SEO";
-import { Centered } from "../styles/global";
+import TextDisplay from "../components/TextDisplay";
 
 export default () => (
   <>
     <SEO title="404" route="/404" />
-    <Centered>
-      <Logo size="medium" />
-      <h1>404: Not found!</h1>
-      <p>
-        The page you're looking for doesn't exist! Please head{" "}
-        <Link to="/">home</Link>.
-      </p>
-    </Centered>
+    <TextDisplay
+      name={"Error 404: Page Not found!"}
+      text={"The page you're looking for doesn't exist!"}
+      displayBack={true}
+    />
   </>
 );
