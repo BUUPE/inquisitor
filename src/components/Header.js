41532a4f7d8601d6f3168b2a7cd8390d7471b5a7
vitorvicente <vitor@bu.edu>
2022-01-04 13:13:37 +0000

    Fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,34 @@
-import React, { useContext } from "react";
+import React from "react";
 import styled from "styled-components";
 
-import Background from "../images/main-background.png"
+import Background from "../images/main-background.png";
+
+const MastHead = styled.div`
+  margin-bottom: 60px;
+  background: no-repeat center center;
+  background-color: #868e96;
+  background-attachment: scroll;
+  position: relative;
+  background-size: cover;
+  background-image: url(${Background});
+`;
 
 const SiteHeading = styled.div`
-	padding:200px 0 150px;
-	height:650px;
-	color:#fff;
-	text-align: center;
-	justify-content: center;
-	& h1 {
-		font-size:50px;
-		margin-top:0
-	}
+  padding: 200px 0 150px;
+  height: 650px;
+  color: #fff;
+  text-align: center;
+  justify-content: center;
+  & h1 {
+    font-size: 50px;
+    margin-top: 0;
+  }
 `;
 
 const Header = () => (
   <MastHead>
-    <SiteHeading>
-    </SiteHeading>
+    <SiteHeading></SiteHeading>
   </MastHead>
 );
 
-export default withFirebase(Header);
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
-import React from "react";
-import styled from "styled-components";
+import React, { Component } from "react";
 
-import {
-  AuthUserContext,
-  withFirebase,
-} from "upe-react-components";
+import { AuthUserContext, withFirebase } from "upe-react-components";
 
 import SEO from "../components/SEO";
 import Loader from "../components/Loader";
@@ -12,27 +8,11 @@ import WelcomeDisplay from "../components/Landing/WelcomeDisplay";
 import ApplicantActionsDisplay from "../components/Landing/ApplicantActionsDisplay";
 import InterviewerActionsDisplay from "../components/Landing/InterviewerActionsDisplay";
 
-const Landing = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  p {
-    max-width: 500px;
-    text-align: center;
-  }
-`;
-
-
 class IndexPage extends Component {
   _initFirebase = false;
   state = {
-		loading: true,
-		denyListed: false,
-    generalSettings: null
+    loading: true,
+    generalSettings: null,
   };
   static contextType = AuthUserContext;
 
@@ -48,7 +28,7 @@ class IndexPage extends Component {
       });
     }
   }
-  
+
   loadData = async () => {
     this._initFirebase = true;
     const loadGeneralSettings = this.props.firebase
@@ -60,79 +40,71 @@ class IndexPage extends Component {
           loading: false,
         })
       );
-			
-    Promise.all([
-      loadGeneralSettings
-    ]).then((values) =>
+
+    Promise.all([loadGeneralSettings]).then((values) =>
       this.setState({
         loading: false,
-        generalSettings: values[0]
+        generalSettings: values[0],
       })
     );
   };
-  
+
   render() {
-		const {
-      loading,
-			denyListed,
-      errorMsg,
-      generalSettings
-    } = this.state;
-		
-		// Data Loading
+    const { loading, generalSettings } = this.state;
+
+    // Data Loading
     if (loading) return <Loader />;
-		
-		// Not Logged In
-		if (!!!this.context)
-			return (
-				<WelcomeDisplay 
-						name={"to Inquisitor"} 
-						text={"It seems that you are not logged in, if you'd like to continue into Inquisitor, please Login below with your BU account."} 
-					/>
-			);
-			
-		// Declare Display Text Variable 
-		let text = "Your application is currently underway, please check below for all the available actions you can take at this time.";
-		
-		// UPE Member
-		if (!!this.context.roles.upemember) {
-			if (!!this.context.roles.recruitmentteam)
-				text = "RECRUITMENT TEAM";
-			else
-				text = "REGULAR MEMBER";
-			
-			return (
-				<>
-					<WelcomeDisplay 
-						name={this.context.name.split(" ")[0]} 
-						text={text} 
-					/>
-					<InterviewerActionsDisplay />
-				</>
-			);
-		}
-		// Denylisted
+
+    // Not Logged In
+    if (!!!this.context)
+      return (
+        <WelcomeDisplay
+          name={"to Inquisitor"}
+          text={
+            "It seems that you are not logged in, if you'd like to continue into Inquisitor, please Login below with your BU account."
+          }
+        />
+      );
+
+    // Declare Display Text Variable
+    let text =
+      "Your application is currently underway, please check below for all the available actions you can take at this time.";
+
+    // UPE Member
+    if (!!this.context.roles.upemember) {
+      if (!!this.context.roles.recruitmentteam) text = "RECRUITMENT TEAM";
+      else text = "REGULAR MEMBER";
+
+      return (
+        <>
+          <WelcomeDisplay name={this.context.name.split(" ")[0]} text={text} />
+          <InterviewerActionsDisplay />
+        </>
+      );
+    }
+    // Denylisted
     else if (!!this.context.roles.denyListed)
-			text = "Unfortunately you are not eligible to apply to Upsilon PI Epsilon. If you think this is a mistake, or want further information on the situation, please contact our EBoard at upe@bu.edu."
-		// Not Applicant
-		else if (!!!this.context.roles.applicant) {
-			// && Apps Closed
-			if (!generalSettings.applicationsOpen)
-				text = "Applications are currently closed, if you'd like to be notified once they are open please sign up for our Newsletter below."
-			// && Apps Open
-			else
-				text = "It seems like you have yet to apply, before continuing you should do so below."
-		}
-	
-		return (
-			<>
-				<WelcomeDisplay 
-					name={this.context.name.split(" ")[0]} 
-					text={text} 
-				/>
-				<ApplicantActionsDisplay />
-			</>
-		);
+      text =
+        "Unfortunately you are not eligible to apply to Upsilon PI Epsilon. If you think this is a mistake, or want further information on the situation, please contact our EBoard at upe@bu.edu.";
+    // Not Applicant
+    else if (!!!this.context.roles.applicant) {
+      // && Apps Closed
+      if (!generalSettings.applicationsOpen)
+        text =
+          "Applications are currently closed, if you'd like to be notified once they are open please sign up for our Newsletter below.";
+      // && Apps Open
+      else
+        text =
+          "It seems like you have yet to apply, before continuing you should do so below.";
+    }
+
+    return (
+      <>
+        <SEO title="Landing" route="/" />
+        <WelcomeDisplay name={this.context.name.split(" ")[0]} text={text} />
+        <ApplicantActionsDisplay />
+      </>
+    );
   }
 }
 
